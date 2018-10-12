2655f7f2f639da381e7943a5b2cd677750d1663c
Ryan <scaventum@gmail.com>
2018-10-13 01:41:19 +1300

    add image on carousel show

--- resources/views/carousels/show.blade.php
+++ resources/views/carousels/show.blade.php
@@ -13,10 +13,17 @@
             </nav>
 
             <div class="card card-body bg-light content-list">
-                <h3>{!!$carousel->caption!!}</h3>
-                <p>{{$carousel->subcaption}}</p>
-                <hr>
-                <small>Last update at {{date("d M Y H:i:s",strtotime($carousel->updated_at))}}</small>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="{{asset('img/carousel/'.$carousel['img'])}}" class="img-fluid img-thumbnail">
+                    </div>
+                    <div class="col-sm-8">
+                        <h3>{!!$carousel->caption!!}</h3>
+                        <p>{{$carousel->subcaption}}</p>
+                        <hr>
+                        <small>Last update at {{date("d M Y H:i:s",strtotime($carousel->updated_at))}}</small>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
